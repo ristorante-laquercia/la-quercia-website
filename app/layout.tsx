8368f97c5b577caca54e1122ee411fdb5c823796
ivanbalmita <ivanbalmita@gmail.com>
2026-06-19 11:28:17 +0200

    Layout page metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export const metadata: Metadata = {
   },
   description:
     'La Quercia è una Ristobirreria a Termoli: cavatelli fatti a mano, cucina alla birra con malto e luppolo, pizza artigianale e selezione guidata da Beer Sommelier.',
+  openGraph: {
+    images: [{ url: '/assets/imgs/og-image.png', width: 1200, height: 630 }],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/assets/imgs/og-image.png'],
+  },
   robots: {
     index: true,
     follow: true,
